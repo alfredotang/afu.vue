cef92ae2d2a824a6fbc04f894f7d60b8530dd7c5
alfredotang <asdzxcca0617@gmail.com>
2021-06-19 00:51:04 +0800

    feat: add Todo pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,20 +4,32 @@ import Home from '@src/views/Home.vue';
 
 Vue.use(VueRouter);
 
-const routes = [
+export const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    exact: true
   },
   {
     path: '/about',
     name: 'About',
+    exact: true,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ '@src/views/About.vue')
+  },
+  {
+    path: '/todo',
+    name: 'Todo',
+    exact: true,
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "todo" */ '@src/views/Todo.vue')
   }
 ];
 
